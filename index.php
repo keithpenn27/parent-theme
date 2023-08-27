0d09cbbb69581117823d7e7d1c16f3137ad64238
Keith Pennington <keith.penn27@gmail.com>
2023-08-27 12:49:45 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* The main template file
+*
+* This is the most generic template file in a WordPress theme
+* and one of the two required files for a theme (the other being style.css).
+* It is used to display a page when nothing more specific matches a query.
+* E.g., it puts together the home page when no home.php file exists.
+*
+* @link https://github.com/keithpenn27
+*
+* @package WordPress
+* @subpackage Parent Theme
+* @since Parent Theme 1.0
+*/
+
+// Silence is golden.
+
+?>
